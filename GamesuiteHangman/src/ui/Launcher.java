f6aee8249bb6d145a9c78dd1b3bd59111b92954c
Marie <maria.verdonck@student.ucll.be>
2017-05-09 22:23:25 +0200

    Maria Verdonck Class: Launcher en UI (exceptions worden nu opgevangen met try catch, user krijgt error message en dan begint methode waar error in gebeurde opnieuw)

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -8,14 +8,25 @@ public class Launcher {
 	private static PictionaryUi ui;
 	
 	public static void main(String[] args) throws DomainException {
-		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		Speler speler = new Speler(naam);
-
+		
+		Speler speler = addSpeler();
+		
 		JOptionPane.showMessageDialog(null, speler.getNaam() + " zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
-		JOptionPane.showMessageDialog(null, speler.getNaam() + " heeft als score: " + speler.getScore(), naam, JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, speler.getNaam() + " heeft als score: " + speler.getScore(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 
 		ui = new PictionaryUi(speler);
 		ui.showMenu();
+	}
+	
+	public static Speler addSpeler() {
+		try {
+			String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
+			Speler speler = new Speler(naam);
+			return speler;
+		} catch (DomainException e) {
+			JOptionPane.showMessageDialog(null,e.getMessage());
+			return addSpeler();
+		}
 	}
 
 }
--- GamesuiteHangman/src/ui/PictionaryUi.java
+++ GamesuiteHangman/src/ui/PictionaryUi.java
@@ -38,47 +38,73 @@ public class PictionaryUi {
 	} 
 	
 	private void askForPunt() throws DomainException {
-		Punt punt = askForPuntCoord();
-		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString());
-		//TODO: add punt to game/speler?
+		try {
+			Punt punt = askForPuntCoord();
+			JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString());
+			//TODO: add punt to game/speler?
+		} catch (DomainException e) {
+			JOptionPane.showMessageDialog(null,e.getMessage());
+			askForPunt();
+		}
 	}
 	
 	private Punt askForPuntCoord() throws DomainException {
-		String xcoord = JOptionPane.showInputDialog("x coordinaat van een punt:");
-		checkParamInt(xcoord);
-		String ycoord = JOptionPane.showInputDialog("y coordinaat van een punt:");
-		checkParamInt(ycoord);
-		Punt punt = new Punt(Integer.parseInt(xcoord), Integer.parseInt(ycoord));
-		return punt;
+		try {
+			String xcoord = JOptionPane.showInputDialog("x coordinaat van een punt:");
+			checkParamInt(xcoord);
+			String ycoord = JOptionPane.showInputDialog("y coordinaat van een punt:");
+			checkParamInt(ycoord);
+			Punt punt = new Punt(Integer.parseInt(xcoord), Integer.parseInt(ycoord));
+			return punt;
+		} catch (DomainException e) {
+			JOptionPane.showMessageDialog(null,e.getMessage());
+			return askForPuntCoord();
+		}
 	}
 	
 	private void askForRechthoek() throws DomainException {
-		JOptionPane.showMessageDialog(null, "Geef coordinaten van linkerbovenhoek punt in.");
-		Punt linkerBovenHoek = askForPuntCoord();
-		String breedte = JOptionPane.showInputDialog("Breedte van de rechthoek:");
-		checkParamInt(breedte);
-		String hoogte = JOptionPane.showInputDialog("Hoogte van de rechthoek:");
-		checkParamInt(hoogte);
-		Rechthoek rechthoek = new Rechthoek(linkerBovenHoek, Integer.parseInt(breedte), Integer.parseInt(hoogte));
-		JOptionPane.showMessageDialog(null, "U heeft een correct rechthoek aangemaakt: " + rechthoek.toString());
-		//TODO: add punt to game/speler?
+		try {
+			JOptionPane.showMessageDialog(null, "Geef coordinaten van linkerbovenhoek punt in.");
+			Punt linkerBovenHoek = askForPuntCoord();
+			String breedte = JOptionPane.showInputDialog("Breedte van de rechthoek:");
+			checkParamInt(breedte);
+			String hoogte = JOptionPane.showInputDialog("Hoogte van de rechthoek:");
+			checkParamInt(hoogte);
+			Rechthoek rechthoek = new Rechthoek(linkerBovenHoek, Integer.parseInt(breedte), Integer.parseInt(hoogte));
+			JOptionPane.showMessageDialog(null, "U heeft een correct rechthoek aangemaakt: " + rechthoek.toString());
+			//TODO: add punt to game/speler?
+		} catch (DomainException e) {
+			JOptionPane.showMessageDialog(null,e.getMessage());
+			askForRechthoek();
+		}
 	}
 	
 	private void askForDriehoek() throws DomainException {
-		JOptionPane.showMessageDialog(null, "Geef coordinaten van eerste hoekpunt in.");
-		Punt hoekPunt1 = askForPuntCoord();
-		JOptionPane.showMessageDialog(null, "Geef coordinaten van tweede hoekpunt in.");
-		Punt hoekPunt2 = askForPuntCoord();
-		JOptionPane.showMessageDialog(null, "Geef coordinaten van derde hoekpunt in.");
-		Punt hoekPunt3 = askForPuntCoord();
-		Driehoek driehoek = new Driehoek(hoekPunt1, hoekPunt2, hoekPunt3);
-		JOptionPane.showMessageDialog(null, "U heeft een correct driehoek aangemaakt: " + driehoek.toString());
-		//TODO: add driehoek to game/speler?
+		try {
+			JOptionPane.showMessageDialog(null, "Geef coordinaten van eerste hoekpunt in.");
+			Punt hoekPunt1 = askForPuntCoord();
+			JOptionPane.showMessageDialog(null, "Geef coordinaten van tweede hoekpunt in.");
+			Punt hoekPunt2 = askForPuntCoord();
+			JOptionPane.showMessageDialog(null, "Geef coordinaten van derde hoekpunt in.");
+			Punt hoekPunt3 = askForPuntCoord();
+			Driehoek driehoek = new Driehoek(hoekPunt1, hoekPunt2, hoekPunt3);
+			JOptionPane.showMessageDialog(null, "U heeft een correct driehoek aangemaakt: " + driehoek.toString());
+			//TODO: add driehoek to game/speler?
+		} catch (DomainException e) {
+			JOptionPane.showMessageDialog(null,e.getMessage());
+			askForDriehoek();
+		}
 	}
 	
 	private void checkParamInt(String coord) throws DomainException {
 		if (coord == null || coord.trim().isEmpty()) {
-			throw new DomainException("Geen (juiste) integer gegeven!");
+			throw new DomainException("Vul alles in.");
+		}
+		//Test if digit with regular expression:
+		// -?     --> negative sign, could have none or one
+		// \\d+   --> one or more digits
+		if (!coord.matches("^?\\d+$")) {
+			throw new DomainException("Geef positieve nummers.");
 		}
 	}
 
