252c19d18df91222e18bd037cdda82fb0e8f19c0
Marie <maria.verdonck@student.ucll.be>
2017-05-09 11:28:46 +0200

    Doener: Maria Verdonck Denker: Joran Trompet US: 2 Class: PictionaryUi

---
+++ GamesuiteHangman/src/ui/PictionaryUi.java
@@ -0,0 +1,28 @@
+package ui;
+
+import domain.Punt;
+import domain.Speler;
+import javax.swing.JOptionPane;
+
+public class PictionaryUi {
+	
+	private Speler speler;
+
+	public PictionaryUi(Speler speler) {
+		super();
+		this.speler = speler;
+	}
+	
+	public void showMenu() {
+		getPunt();
+	}
+	
+	private void getPunt() {
+		String xcoord = JOptionPane.showInputDialog("x coordinaat van een punt:");
+		String ycoord = JOptionPane.showInputDialog("y coordinaat van een punt:");
+		Punt punt = new Punt(Integer.parseInt(xcoord), Integer.parseInt(ycoord));
+		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString());
+		//TODO: add punt to game/speler?
+	}
+
+}
