68028e588e0af90de570b48c976b965c75c07c00
Marie <maria.verdonck@student.ucll.be>
2017-05-09 12:08:04 +0200

    Throw domainexception if coord punt empty

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -11,7 +11,7 @@ public class Launcher {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
 		Speler speler = new Speler(naam);
 
-		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, speler.getNaam() + " zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 		JOptionPane.showMessageDialog(null, speler.getNaam() + " heeft als score: " + speler.getScore(), naam, JOptionPane.INFORMATION_MESSAGE);
 
 		ui = new PictionaryUi(speler);
--- GamesuiteHangman/src/ui/PictionaryUi.java
+++ GamesuiteHangman/src/ui/PictionaryUi.java
@@ -1,7 +1,11 @@
 package ui;
 
+import domain.DomainException;
 import domain.Punt;
 import domain.Speler;
+
+import java.awt.HeadlessException;
+
 import javax.swing.JOptionPane;
 
 public class PictionaryUi {
@@ -13,17 +17,25 @@ public class PictionaryUi {
 		this.speler = speler;
 	}
 	
-	public void showMenu() {
-		getPunt();
+	public void showMenu() throws DomainException {
+		askForPuntCoord();
 		//TODO: Menu van maken
 	}
 	
-	private void getPunt() {
+	private void askForPuntCoord() throws DomainException {
 		String xcoord = JOptionPane.showInputDialog("x coordinaat van een punt:");
+		checkCoord(xcoord);
 		String ycoord = JOptionPane.showInputDialog("y coordinaat van een punt:");
+		checkCoord(ycoord);
 		Punt punt = new Punt(Integer.parseInt(xcoord), Integer.parseInt(ycoord));
 		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString());
 		//TODO: add punt to game/speler?
+	}
+	
+	private void checkCoord(String coord) throws DomainException {
+		if (coord == null || coord.trim().isEmpty()) {
+			throw new DomainException("Coordinaat niet juist!");
+		}
 	}
 
 }
