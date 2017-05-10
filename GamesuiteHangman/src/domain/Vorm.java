1d85a634ddad61554b63b970fcd01c97d788e68b
Marie <maria.verdonck@student.ucll.be>
2017-05-10 10:41:23 +0200

    Maria Verdonck / Arno Piersoul Us: 9 Klasse: driehoek, driehoektest (getomhullende methode en test voor driehoek)

--- GamesuiteHangman/src/domain/Vorm.java
+++ GamesuiteHangman/src/domain/Vorm.java
@@ -1,5 +1,6 @@
 package domain;
 public abstract class Vorm {
+	
 	public abstract boolean equals(Object object);
 	public abstract String toString();
 	public abstract Omhullende getOmhullende() throws DomainException;
